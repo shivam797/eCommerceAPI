95095b0d0a530b5fc43f4b406861745615596b5a
shivam797 <shivamkumar004667@gmail.com>
2023-07-15 22:48:19 +0530

    changes made related to dotenv

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0/eCommerce');
+// mongoose.connect('mongodb://0.0.0.0/eCommerce');
+mongoose.connect(process.env.MongoUri);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console,'error in connecting db'));
